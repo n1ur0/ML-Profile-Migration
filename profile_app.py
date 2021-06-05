0356ea167c7abbaad4ef86f5bd68ed841f6ed64d
Emanuel Oliveira <e.lima.oliv@outlook.com>
2021-06-05 20:33:11 +0100

    remove keras lib

--- profile_app.py
+++ profile_app.py
@@ -85,10 +85,6 @@ from sklearn.metrics import classification_report
 import seaborn as sns
 from itertools import cycle, islice
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
